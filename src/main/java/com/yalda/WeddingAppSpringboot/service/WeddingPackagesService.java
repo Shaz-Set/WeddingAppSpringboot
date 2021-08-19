5b00d47cca0ea60091ff397897d4d169f1c3316c
Shaz-Set <yalda.setoodeh@ust-global.com>
2021-08-19 14:13:45 +1000

    added the required method in the service layer

--- src/main/java/com/yalda/WeddingAppSpringboot/service/WeddingPackagesService.java
+++ src/main/java/com/yalda/WeddingAppSpringboot/service/WeddingPackagesService.java
@@ -1,9 +1,28 @@
 package com.yalda.WeddingAppSpringboot.service;
 
+import com.yalda.WeddingAppSpringboot.model.AddOn;
+import com.yalda.WeddingAppSpringboot.model.DrinkPackage;
 import com.yalda.WeddingAppSpringboot.model.WeddingPackage;
 
 import java.util.List;
 
 public interface WeddingPackagesService {
+     //get
      List<WeddingPackage> getAllWeddingPackages();
+     List<DrinkPackage> getAllDrinkPackages();
+     List<AddOn> getAllAddOns();
+     //update
+     int updateWeddingPackage(WeddingPackage weddingPackage);
+     int updateDrinkPackage(DrinkPackage drinkPackage);
+     int updateAddOn(AddOn addOn);
+     //delete
+     void deleteWeddingPackage(Long id);
+     void deleteDrinkPackage(Long id);
+     void deleteAddOn(Long id);
+     //find
+     WeddingPackage getWeddingPackageById(long id);
+     DrinkPackage getDrinkPackageById(long id);
+     AddOn getAddOnById(long id);
+
+     void calculateTotalPrice(WeddingPackage wp, DrinkPackage dp, AddOn ao, int guests);
 }
--- src/main/java/com/yalda/WeddingAppSpringboot/service/WeddingPackagesServiceImpl.java
+++ src/main/java/com/yalda/WeddingAppSpringboot/service/WeddingPackagesServiceImpl.java
@@ -1,19 +1,105 @@
 package com.yalda.WeddingAppSpringboot.service;
 
+import com.yalda.WeddingAppSpringboot.model.AddOn;
+import com.yalda.WeddingAppSpringboot.model.DrinkPackage;
 import com.yalda.WeddingAppSpringboot.model.WeddingPackage;
+import com.yalda.WeddingAppSpringboot.repository.AddOnRepository;
+import com.yalda.WeddingAppSpringboot.repository.DrinkPackageRepository;
 import com.yalda.WeddingAppSpringboot.repository.WeddingPackagesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WeddingPackagesServiceImpl implements WeddingPackagesService {
 
     @Autowired
     private WeddingPackagesRepository weddingPackagesRepository;
-
+    private DrinkPackageRepository drinkPackageRepository;
+    private AddOnRepository addOnRepository;
+    //get
     @Override
     public List<WeddingPackage> getAllWeddingPackages() {
         return weddingPackagesRepository.findAll();
+    }
+    @Override
+    public List<DrinkPackage> getAllDrinkPackages() {
+        return drinkPackageRepository.findAll();
+    }
+    @Override
+    public List<AddOn> getAllAddOns() {
+        return addOnRepository.findAll();
+    }
+    //update
+    @Override
+    public int updateWeddingPackage(WeddingPackage weddingPackage) {
+        weddingPackagesRepository.save(weddingPackage);
+        return 1;
+    }
+    @Override
+    public int updateDrinkPackage(DrinkPackage drinkPackage) {
+        drinkPackageRepository.save(drinkPackage);
+        return 1;
+    }
+    @Override
+    public int updateAddOn(AddOn addOn) {
+        addOnRepository.save(addOn);
+        return 1;
+    }
+    //delete
+    @Override
+    public void deleteWeddingPackage(Long id) {
+        weddingPackagesRepository.deleteById(id);
+    }
+    @Override
+    public void deleteDrinkPackage(Long id) {
+        drinkPackageRepository.deleteById(id);
+    }
+    @Override
+    public void deleteAddOn(Long id) {
+        addOnRepository.deleteById(id);
+    }
+    //find
+    @Override
+    public WeddingPackage getWeddingPackageById(long id) {
+        Optional< WeddingPackage > optional = weddingPackagesRepository.findById(id);
+        WeddingPackage wp = null;
+        if (optional.isPresent()) {
+            wp = optional.get();
+        } else {
+            throw new RuntimeException(" Employee not found for id :: " + id);
+        }
+        return wp;
+    }
+    @Override
+    public DrinkPackage getDrinkPackageById(long id) {
+        Optional< DrinkPackage > optional = drinkPackageRepository.findById(id);
+        DrinkPackage dp = null;
+        if (optional.isPresent()) {
+            dp = optional.get();
+        } else {
+            throw new RuntimeException(" Employee not found for id :: " + id);
+        }
+        return dp;
+    }
+    @Override
+    public AddOn getAddOnById(long id) {
+        Optional< AddOn > optional = addOnRepository.findById(id);
+        AddOn ao = null;
+        if (optional.isPresent()) {
+            ao = optional.get();
+        } else {
+            throw new RuntimeException(" Employee not found for id :: " + id);
+        }
+        return ao;
+    }
+    //total price
+    @Override
+    public void calculateTotalPrice(WeddingPackage wp, DrinkPackage dp, AddOn ao, int guests) {
+            //calculating the total price based on the quantity of the guests,
+            // wedding package, add-ons, and drink package chosen by the user
+            double totalPrice = (getWeddingPackageById(wp.getId()).getPrice() * guests) +
+                    (getDrinkPackageById(dp.getId()).getPrice() * guests) + getAddOnById(ao.getId()).getPrice();
     }
 }
